79240dcc70a6313fe267f4bbbe0463fcfa373ec6
Jose Herrera <l21212344@tectijuana.edu.mx>
2025-12-08 17:40:39 -0800

    Add files via upload

---
+++ src/PatronesU5v2/bin/LluviaAscii/Dominio/ComportamientoLluviaNormal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronesU5v2
+{
+    public class ComportamientoLluviaNormal : IComportamientoLluvia
+    {
+        private readonly Random _aleatorio;
+
+        public ComportamientoLluviaNormal()
+        {
+            _aleatorio = new Random();
+        }
+
+        public void ActualizarGotas(List<GotaLluvia> gotas, IFabricaGotas fabrica, int ancho, int alto)
+        {
+            for (int i = 0; i < gotas.Count; i++)
+            {
+                gotas[i].Avanzar();
+            }
+
+            for (int i = gotas.Count - 1; i >= 0; i--)
+            {
+                if (gotas[i].Fila >= alto)
+                {
+                    gotas.RemoveAt(i);
+                }
+            }
+
+            int nuevas = _aleatorio.Next(2, 4);
+            for (int i = 0; i < nuevas; i++)
+            {
+                var nuevaGota = fabrica.CrearGota(ancho, alto);
+                gotas.Add(nuevaGota);
+            }
+        }
+    }
+}
---
+++ src/PatronesU5v2/bin/LluviaAscii/Dominio/IFabricaGotas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronesU5v2
+{
+    public interface IFabricaGotas
+    {
+        GotaLluvia CrearGota(int anchoConsola, int altoConsola);
+    }
+}
