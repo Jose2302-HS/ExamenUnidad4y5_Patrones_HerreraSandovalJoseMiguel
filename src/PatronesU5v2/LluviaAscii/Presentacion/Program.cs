ab8a0630b9679ba132e9d5f03cea0c37ae0271b4
Jose Herrera <l21212344@tectijuana.edu.mx>
2025-12-08 17:43:57 -0800

    Add files via upload

---
+++ src/PatronesU5v2/LluviaAscii/Presentacion/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PatronesU5v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Lluvia ASCII con patrones";
+            Console.CursorVisible = false;
+
+            int ancho = Console.WindowWidth;
+            int alto = Console.WindowHeight - 1;
+
+            var gotas = new List<GotaLluvia>();
+
+            IFabricaGotas fabrica = new FabricaGotasBasica();
+            IComportamientoLluvia comportamiento = new ComportamientoLluviaNormal();
+            var renderizador = new RenderizadorLluvia();
+
+            renderizador.InicializarConsola();
+
+            Console.WriteLine("Lluvia ASCII");
+            Console.WriteLine("Presiona ESC para salir.");
+            Thread.Sleep(1000);
+
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    var tecla = Console.ReadKey(true);
+                    if (tecla.Key == ConsoleKey.Escape)
+                    {
+                        break;
+                    }
+                }
+
+                comportamiento.ActualizarGotas(gotas, fabrica, ancho, alto);
+
+                renderizador.DibujarCuadro(gotas, ancho, alto);
+
+                Thread.Sleep(50);
+            }
+
+            Console.CursorVisible = true;
+        }
+    }
+}
